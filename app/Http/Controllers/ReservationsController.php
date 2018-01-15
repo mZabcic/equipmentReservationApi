b0b74c9c2415b3377afea54332ae91e0d38d8076
Mislav Žabčić <mislavz@in2.hr>
2018-01-15 17:19:17 +0100

    Dodana mogucnost zatrazivanja produzetka

--- app/Http/Controllers/ReservationsController.php
+++ app/Http/Controllers/ReservationsController.php
@@ -616,7 +616,6 @@ if (count($check) != 0) {
 
      
 private function checkIfItemsTaken($start_date, $end_date, $items) {
-    return response()->json($items, 400);
       $returnData = [];
       foreach ($items as $item) {
          $data = Item::with('reservations')->where('id', $item)->firstOrFail();
