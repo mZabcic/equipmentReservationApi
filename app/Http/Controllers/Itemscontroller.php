c60252fac4b29167842ac0a20f60c73331ca3df0
Mislav Žabčić <mislavz@in2.hr>
2018-01-21 14:14:47 +0100

    popravak

--- app/Http/Controllers/Itemscontroller.php
+++ app/Http/Controllers/Itemscontroller.php
@@ -686,7 +686,7 @@ if ($check == 0) {
     $items = Item::with("kit")->with("subtype")->with("type")->with("deviceType")->with('reservations')->where('id', $id)->firstOrFail();
    
     $items->reservations = $items->reservations->filter(function ($value, $key) use ($today) {
-      if ($key->status_id != 2)
+      if ($value->status_id != 2)
          return false;
       });
      dd($items->reservations);
