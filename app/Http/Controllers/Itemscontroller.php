4d3ecccc6c3ddd3ba6dd8039fc78e655257fb639
Mislav Žabčić <mislavz@in2.hr>
2018-01-21 14:18:16 +0100

    popravak

--- app/Http/Controllers/Itemscontroller.php
+++ app/Http/Controllers/Itemscontroller.php
@@ -685,10 +685,10 @@ if ($check == 0) {
       $today = new DateTime();
     $items = Item::with("kit")->with("subtype")->with("type")->with("deviceType")->with('reservations')->where('id', $id)->firstOrFail();
    
-    $items->reservations = $items->reservations->filter(function ($value, $key) use ($today) {
-      if ($value->status_id != 2) {
-         return false;
-      }
+    $items->reservations->filter(function ($value, $key) use ($today) {
+     
+         return $value->status_id == 2;
+      
       });
       dd($items->reservations);
      
