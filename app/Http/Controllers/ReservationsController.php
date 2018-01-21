7f8f5cbf46db3b5fe6ffe9c62929475ca45215da
Mislav Žabčić <mislavz@in2.hr>
2018-01-21 10:56:59 +0100

    popravak

--- app/Http/Controllers/ReservationsController.php
+++ app/Http/Controllers/ReservationsController.php
@@ -731,11 +731,7 @@ private function checkIfItemsTaken($start_date, $end_date, $items) {
       $returnData = [];
       foreach ($items as $item) {
          $data = Item::with('reservations')->where('id', $item)->firstOrFail();
-       //  dd($data);
-       $data = $data->filter(function ($value, $key) use ($start_date, $end_date) {
-        if ($value->working == false)
-           return false;
-        });
+      
         $data->reservations = $data->reservations->filter(function ($value, $key) use ($start_date, $end_date) {
         if ($value->status_id != 2)
            return false;
